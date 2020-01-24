47141edc8a5f50033e7a4326821f6022e2755d53
Liz <admin@elizabethmbank.info>
2020-01-24 10:46:51 -0500

    added api call to app--still not working though.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './components/header/header.component';
 import CurrentWeather from './components/current-weather/current-weather.component';
+import {secretKey} from './apiKeys';
 
 import './App.css';
 
-class Weather extends React.Component {
-  constructor(props) {
-    super(props);
+class App extends Component {
+  constructor() {
+    super();
+    
     this.state = {
-      weather: {
-        latitude: "",
-        longitude: "",
-        currently: {
-          summary: "",
-          icon: "",
-          precipProbability: 0,
-          precipType: "",
-          temperature: 0,
-          humidity: 0,
-          pressure: 0,
-        },
-        daily: {
-          summary: "",
-          icon: "",
-          sunriseTime: 0,
-          sunsetTime: 0,
-          moonPhase: 0,
-          precipProbability: 0,
-          precipType: "",
-          temperatureHigh: 0,
-          temperatureLow: 0,
-          humidity: 0,
-        },
-        alerts: []
-      }
+      weather: {},
     };
   }
-}
+
+  
+
+  componentDidMount() {
+    const api = 'https://api.darksky.net/forecast/';
+    const placeLat = 33.9887165;
+    const placeLong = -83.8979573;
+    const queryParam = '?exclude=minutely,hourly,flags';
 
 
-function App() {
-  const secretKey = '563e6c45583f29fa15501a4d8669fab9';
-  return (
-    <div className="App">
-      <Header  />
-      <CurrentWeather />
-    </div>
-  );
+    fetch(api + secretKey + placeLat + placeLong + queryParam, {
+      method: 'GET',
+      mode: 'no-cors'
+    })
+    .then(response => console.log(response.json()))
+    //.then(weather => this.setState( {weather} ))
+    // .catch(function(error) {
+    //   console.log('Looks like there was a problem: \n', error);
+    // })
+  };
+
+  //console.log(this.state.weather);
+
+  
+  render() {
+    return (
+      <div className="App">
+        <Header  />
+        <CurrentWeather />
+      </div>
+    );
+  }
+
 }
 
 export default App;
